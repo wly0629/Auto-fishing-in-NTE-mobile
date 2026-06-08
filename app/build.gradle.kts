c080c1da7a0f6904281b55b0782e9f44679d5496
AutoFish <local@autofish.local>
2026-06-08 19:31:16 +0800

    初始版本发布 v1.0.0 (versionCode=1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.smallrong.autoclicker"
         minSdk = 28
         targetSdk = 34
-        versionCode = 5
+        versionCode = 1
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
